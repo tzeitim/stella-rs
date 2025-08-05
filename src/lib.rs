fc84b93a9c88f2c3fbf29ad2d54d990b1749d09b
tzeitim <pedroy.final@gmail.com>
2025-08-05 12:27:46 +0300

    renamed package to stellars

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+use pyo3::prelude::*;
+
+#[pyfunction]
+fn hello_stella() -> PyResult<String> {
+    Ok("Hello from stella-rs! This is a stub implementation.".to_string())
+}
+
+#[pyfunction]
+fn version() -> PyResult<String> {
+    Ok("0.1.0".to_string())
+}
+
+#[pymodule]
+fn stellars(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(hello_stella, m)?)?;
+    m.add_function(wrap_pyfunction!(version, m)?)?;
+    Ok(())
+}
